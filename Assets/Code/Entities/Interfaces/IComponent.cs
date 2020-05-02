adb020460042132bb2eacbc046d073284747c8d9
Seth Hamm <danksupercow@gmail.com>
2020-05-01 21:51:14 -0500

    Update Clean Slate - 5/1/20
    Removed failed attempt at a homebrew ECS, and Rebased the branch on Testing so it is up to date

--- Assets/Code/Entities/Interfaces/IComponent.cs
+++
@@ -1,9 +0,0 @@
-﻿public interface IComponent
-{
-    EntityComponent Container { get; set; }
-    bool Active { get; set; }
-
-    void OnAdd();
-    void OnDestroy();
-    void OnUpdate(float deltaTime);
-}
--- Assets/Code/Entities/Interfaces/IIdentification.cs
+++
@@ -1,4 +0,0 @@
-﻿public interface IIdentification
-{
-    int ID { get; set; }
-}
