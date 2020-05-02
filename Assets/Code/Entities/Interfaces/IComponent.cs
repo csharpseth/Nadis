5b9de92226ce300a239622e863f9299d05a52068
Seth Hamm <danksupercow@gmail.com>
2020-05-01 21:45:44 -0500

    Custom Entity Component System - Started
    I started to write a mild Entity Component system however it is too big of a task for the results it would give me with this state of the project so I am branching off so I still have reference to it

---
+++ Assets/Code/Entities/Interfaces/IComponent.cs
@@ -0,0 +1,9 @@
+﻿public interface IComponent
+{
+    EntityComponent Container { get; set; }
+    bool Active { get; set; }
+
+    void OnAdd();
+    void OnDestroy();
+    void OnUpdate(float deltaTime);
+}
